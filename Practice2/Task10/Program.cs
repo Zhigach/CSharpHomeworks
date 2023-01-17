c49abeed1cf110b94677cc93091bd2c449b81d82
Alexander Zhigachev <sashazhig@gmail.com>
2023-01-17 22:04:53 +0300

    Task10 from second practice done

---
+++ Practice2/Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetNumberFromUser(){
+    Console.WriteLine("Enter three digit integer number (like 111, 123, 987, etc.)");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int GetSecondDigit(int number){
+    int secondDigit = (number % 100) / 10;
+    return secondDigit;
+}
+
+int input = GetNumberFromUser();
+int secondDigit = GetSecondDigit(input);
+Console.WriteLine($"The second digit of number {input} is {secondDigit}");
+
