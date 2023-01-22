206f122a2687282811a31c1b9a51aef5a5a7f17c
Alexander Zhigachev <sashazhig@gmail.com>
2023-01-22 21:48:44 +0300

    Task 23 done and tested

---
+++ Practice3/Task23/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int getNumberFromUser(string message){
+    int result = 0;
+    while (true){
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 1){
+            break;
+        }            
+        else{
+            Console.WriteLine("You've entered incorrect number");
+        }
+    }
+    return result;
+}
+
+void printCubes(int a){
+    for (int i = 1; i <= a-1; i++){
+        Console.Write(Math.Pow(i, 3) + ", ");
+    }
+    Console.WriteLine(Math.Pow(a, 3));
+}
+
+int N = getNumberFromUser("Enter an integer number (1 and bigger)");
+printCubes(N);
