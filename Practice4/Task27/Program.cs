a1bdb4d4550921eedee1b676991f60475f326e81
Alexander Zhigachev <sashazhig@gmail.com>
2023-01-26 09:51:12 +0300

    Task27 done and tested

---
+++ Practice4/Task27/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int getNumberFromUser(string message, int lowest){
+    int result = 0;
+    while (true){
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result >= lowest){
+            break;
+        } else {
+            Console.WriteLine($"You've entered incorrect number (must be higher than {lowest-1}). Try again.");
+        }
+    }
+    return result;
+}
+
+int sumDigits(int number){
+    int result = 0;
+    while (number > 0){
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int n = getNumberFromUser("Enter an integer number", 0);
+
+Console.WriteLine($"Summ of digits in {n} is {sumDigits(n)}");
